2ba84f719b09adf27b129f4ed3b538a6f4bb73e3
SllimKrakken <millst83@gmail.com>
2024-12-18 12:39:48 -0800

    cleaning edits

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -61,36 +61,36 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "--output_artifact", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type = str,
+        help = "Artifact name"
         required = True
     )
 
     parser.add_argument(
         "--output_type", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type = str, 
+        help = "Artifact type"
         required = True
     )
 
     parser.add_argument(
         "--output_description", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type = str,
+        help = "Description"
         required = True
     )
 
     parser.add_argument(
         "--min_price", 
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type = float
+        help = "Minimum price"
         required = True
     )
 
     parser.add_argument(
         "--max_price",
-        type = ## INSERT TYPE HERE: str, float or int,
-        help = ## INSERT DESCRIPTION HERE,
+        type = float
+        help = "Max Price"
         required = True
     )
 
